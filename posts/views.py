d6fa5fa1f3b978cbc32731ffc5f05b72d1e3045a
Chris Grabham <chrisgrabham@outlook.com>
2024-02-28 15:23:32 +0000

    Add initial fuctionality to posts app

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Posts
-
+from .forms import PostsForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,9 +9,25 @@ from .models import Posts
 def all_posts(request):
     """Show all posts"""
 
+    form = PostsForm()
+
+    if request.method == "POST":
+        form = PostsForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            messages.success(request, 'Post added successfully')
+            return redirect('posts')
+        else:
+            messages.error(request,
+                           'Update failed. Please ensure the form is valid.')
+
     posts = Posts.objects.all()
+
     context = {
         'posts': posts,
+        'form': form,
     }
 
     return render(request, 'posts/posts.html', context)
