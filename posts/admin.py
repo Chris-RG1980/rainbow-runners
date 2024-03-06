9a213e5d86d8a2f44d7041907339c99749db5a27
Chris Grabham <chrisgrabham@outlook.com>
2024-03-06 18:36:39 +0000

    Register Comment model

--- posts/admin.py
+++ posts/admin.py
@@ -5,3 +5,20 @@ from .models import Posts
 
 
 admin.site.register(Posts)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('sku',
+                    'name',
+                    'category',
+                    'price',
+                    'key_features',
+                    'material',
+                    'care_instructions',
+                    'colour',
+                    'image'
+                    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'name')
