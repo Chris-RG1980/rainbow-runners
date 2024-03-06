54894ab49030f12de14d5d616b554419cf2c429d
Chris Grabham <chrisgrabham@outlook.com>
2024-03-06 18:37:43 +0000

    Register Comment model

--- posts/admin.py
+++ posts/admin.py
@@ -1,24 +1,8 @@
 from django.contrib import admin
-from .models import Posts
+from .models import Posts, Comment
 
 # Register your models here.
 
 
 admin.site.register(Posts)
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('sku',
-                    'name',
-                    'category',
-                    'price',
-                    'key_features',
-                    'material',
-                    'care_instructions',
-                    'colour',
-                    'image'
-                    )
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('friendly_name', 'name')
+admin.site.register(Comment)
