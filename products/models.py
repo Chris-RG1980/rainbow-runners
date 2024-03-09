79f254dd77ea5dd22bc60976ef8381c71f6e3d24
Chris Grabham <chrisgrabham@outlook.com>
2024-03-09 15:02:25 +0000

    Add json string helper methods

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import json
 
 # Create your models here.
 
@@ -19,17 +20,68 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.SET_NULL
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
     )
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    sku = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
+    name = models.CharField(
+        max_length=254
+    )
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    key_features = models.TextField(null=True, blank=True)
-    material = models.CharField(max_length=100, null=True, blank=True)
-    care_instructions = models.TextField(null=True, blank=True)
-    colour = models.CharField(max_length=254, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+    key_features = models.TextField(
+        null=True,
+        blank=True
+    )
+    material = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+    care_instructions = models.TextField(
+        null=True,
+        blank=True
+    )
+    colour = models.TextField(
+        null=True,
+        blank=True
+    )
+    image = models.ImageField(
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
+
+    # Helper method to serialize a list of features into a JSON string
+    def set_key_features(self, key_features_json):
+        self.key_features = key_features_json
+
+    # Helper method to deserialize the features JSON string
+    def get_key_features(self):
+        try:
+            return json.loads(self.key_features) if self.key_features else []
+        except json.JSONDecodeError:
+            return []
+
+    # Helper method to serialize care instructions into a JSON string
+    def set_care_instructions(self, care_instructions_json):
+        self.care_instructions = care_instructions_json
+
+    # Helper method to deserialize the care instructions JSON string
+    def get_care_instructions(self):
+        try:
+            return json.loads(
+                self.care_instructions) if self.care_instructions else []
+        except json.JSONDecodeError:
+            return []
