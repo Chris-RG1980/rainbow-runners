550930f96546341a8009d9191eaeb99fc5ce91d5
Chris Grabham <chrisgrabham@outlook.com>
2024-03-11 16:43:29 +0000

    Refactor total, add subtotal variable

--- bag/contexts.py
+++ bag/contexts.py
@@ -13,12 +13,15 @@ def bag_contents(request):
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        subtotal = quantity * product.price
+        total += subtotal
         product_count += quantity
+
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'subtotal': subtotal,
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
