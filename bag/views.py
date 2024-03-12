4a53dd171a0919ec6582f4c58f64b87c3d2f1c3f
Chris Grabham <chrisgrabham@outlook.com>
2024-03-12 11:03:33 +0000

    Adjust add_to_bag to handle product_size

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -12,15 +15,40 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+                msg = (f'Updated size {size.upper()} '
+                       f'{product.name} quantity to '
+                       f'{bag[item_id]["items_by_size"][size]}')
+                messages.success(request, msg)
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+                msg = (f'Added size {size.upper()} '
+                       f'{product.name} to your bag')
+                messages.success(request, msg)
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+            msg = (f'Added size {size.upper()} {product.name} to your bag')
+            messages.success(request, msg)
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+            msg = (f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(request, msg)
+        else:
+            bag[item_id] = quantity
+            msg = (f'Added {product.name} to your bag')
+            messages.success(request, msg)
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
