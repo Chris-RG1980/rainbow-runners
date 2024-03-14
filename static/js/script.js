6cfd9393e38656249fe5bfd8b620fcfa458fa0d6
Chris Grabham <chrisgrabham@outlook.com>
2024-03-14 20:17:06 +0000

    Add tooltip comment

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// Bootstrap Tooltip
 document.addEventListener('DOMContentLoaded', function () {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
