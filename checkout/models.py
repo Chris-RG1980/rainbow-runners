af1e540f527d040634b2db64a6f3781dbbdbbc10
Chris Grabham <chrisgrabham@outlook.com>
2024-03-22 13:49:54 +0000

    Add user_profile to Order model

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.conf import settings
 from django.db import models
 from django.db.models import Sum
+from profiles.models import UserProfile
 
 from products.models import Product
 
@@ -11,6 +12,13 @@ from products.models import Product
 
 
 class Order(models.Model):
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders'
+    )
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
