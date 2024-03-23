f6c8e1d040304acbc953455f6ecb136ce388448a
Chris Grabham <chrisgrabham@outlook.com>
2024-03-23 14:28:14 +0000

    Add profile update functionality

--- profiles/models.py
+++ profiles/models.py
@@ -13,18 +13,13 @@ class UserProfile(models.Model):
     """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_full_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+    )
     default_phone_number = models.CharField(
         max_length=20,
-        null=True,
-        blank=True
-    )
-    default_postcode = models.CharField(
-        max_length=20,
-        null=True,
-        blank=True
-    )
-    default_town_or_city = models.CharField(
-        max_length=40,
         null=True,
         blank=True
     )
@@ -38,8 +33,18 @@ class UserProfile(models.Model):
         null=True,
         blank=True
     )
+    default_town_or_city = models.CharField(
+        max_length=40,
+        null=True,
+        blank=True
+    )
     default_county = models.CharField(
         max_length=80,
+        null=True,
+        blank=True
+    )
+    default_postcode = models.CharField(
+        max_length=20,
         null=True,
         blank=True
     )
