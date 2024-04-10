eda80085c0f61d288e279bf7acabb4be2b551525
Chris Grabham <chrisgrabham@outlook.com>
2024-04-10 19:26:23 +0100

    Remove print statements

--- rainbow_runners/startup.py
+++ rainbow_runners/startup.py
@@ -19,12 +19,8 @@ def assign_users_to_coordinators():
     coordinator_usernames = ['Chris', 'stuart']
 
     for username in coordinator_usernames:
-        try:
-            user = User.objects.get(username=username)
-            user.groups.add(group)
-            print(f"Added {username} to co-ordinators")
-        except User.DoesNotExist:
-            print(f"User {username} does not exist")
+        user = User.objects.get(username=username)
+        user.groups.add(group)
 
 
 def assign_users_to_admin():
@@ -34,12 +30,8 @@ def assign_users_to_admin():
     admin_usernames = ['Chris', 'adam1']
 
     for username in admin_usernames:
-        try:
-            user = User.objects.get(username=username)
-            user.groups.add(group)
-            print(f"Added {username} to Admin")
-        except User.DoesNotExist:
-            print(f"User {username} does not exist")
+        user = User.objects.get(username=username)
+        user.groups.add(group)
 
 
 assign_users_to_coordinators()
