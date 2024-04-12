2214553e3507abc866891898bb089c21e16c4898
Chris Grabham <chrisgrabham@outlook.com>
2024-04-12 16:36:04 +0100

    Add max length to question

--- contact/forms.py
+++ contact/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label="Your Name", max_length=100)
     email = forms.EmailField(label="Email Address", max_length=150)
-    question = forms.CharField(widget=forms.Textarea)
+    question = forms.CharField(widget=forms.Textarea, max_length=300)
