49adc27254ea6317da84a0b826f8c57e548b4d3f
Chris Grabham <chrisgrabham@outlook.com>
2024-04-18 18:33:37 +0100

    Amend if statement for emails

--- rainbow_runners/settings.py
+++ rainbow_runners/settings.py
@@ -231,7 +231,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Settings for Email Confirmation
 
 # Email settings for development
-if DEBUG:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'rainbowrunners@example.com'
 else:
@@ -240,7 +240,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    DEFAULT_FROM_EMAIL = 'Rainbow Runners'
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
 
